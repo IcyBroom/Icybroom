bf16bedf46b9b04be145a010d7549b7199e92f89
anoreldaim <anoreldaim@uiowa.edu>
2023-02-02 21:16:56 -0600

    Update schedule.js

--- app/schedule/schedule.js
+++ app/schedule/schedule.js
@@ -158,8 +158,8 @@ export default function Schedule() {
                         </label> */}
                         <button type="button" onClick = { toggleDay }className="ml-2 w-14 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 h-8 text-center text-justify-center mb-2"><div className = "relative  bottom-1 right-3" >Toggle</div></button>
                     </div>
-                    <div className = "mr-12"> Time: {time} </div>
-                    <div className = "cursor-pointer">
+                    <div className = "mr-12 "> {time} </div>
+                    <div>
                         Time Left: 
                     </div>
                 </div>
